168b50eb1f27111149e93a26d665b7482ebe1261
suraj-mandal <suraj.mandal738@gmail.com>
2026-04-04 02:01:59 +0530

    Fixed port env

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -92,7 +92,7 @@ DATABASES = {
         "USER"    : env.str("POSTGRES_USER"),
         "PASSWORD": env.str("POSTGRES_PASSWORD"),
         "HOST"    : env.str("POSTGRES_HOST"),
-        "PORT"    : env.int("POSTGRES_POST", default=5432),
+        "PORT"    : env.int("POSTGRES_PORT", default=5432),
     }
 }
 
